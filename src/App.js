dc4bcc7bce2d86ca814249a17145d735896a89bd
Long-Qin <mrmrmr.wayne@gmail.com>
2023-09-21 17:51:53 -0400

    Designed main UI interface with Search and List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Button, Input, List, Card, message } from "antd";
+import { useState } from 'react';
+import { getContractNFTs } from "./uils";
+import "./App.css";
+
+const { Header, Content } = Layout;
 
 function App() {
+  const [nfts, setNfts]  = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = async () => {
+    if (searchText === "") {
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const data = await getContractNFTs(searchText);
+      setNfts(data.result);
+    } catch (error) {
+      message.error(error.message)
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Layout style={{ height: "100vh"}}>
+      <Header>
+        <div style={{ fontSize: 16, fontWeight: 600, color: "white"}}>NFT UI</div>
+      </Header>
+      <Content
+        style={{ height: "calc(100% - 64px)", padding: 20, overflowY: "auto"}}
+      >
+
+        <Input.Group compact>
+          <Input 
+            style={{ width: 500}}
+            placeholder="Enter a NFT contract address to search"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <Button type="primary" onClick={handleSearch}>Search</Button>
+        </Input.Group>
+        <List 
+          style={{
+            marginTop: 20,
+            height: "calc(100% - 52px)",
+            overflow: "auto",
+          }}
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 3,
+            md: 3,
+            lg: 3,
+            xl: 4,
+            xxl: 4,
+          }}
+          dataSource={[1, 2, 3, 4, 5]}
+          renderItem={(nft) => (
+            <List.Item key={nft}>
+              <Card title={nft} />
+            </List.Item>
+          )}
+        />
+      </Content>
+    </Layout>
+  )
 }
 
 export default App;
